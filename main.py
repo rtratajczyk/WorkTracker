3eeddd6c6b78d1bae36cdd733840e81b6040acdd
Rafał Ratajczyk <r.t.ratajczyk@gmail.com>
2021-10-19 22:26:25 +0200

    Created class "Task", implemented drafts of new functions.

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 import tkinter as tk
-from datetime import datetime
+import time
 
 window = tk.Tk()
 window.title("WorkTracker")
 # window.columnconfigure
 
+
+class task:
+    """The task class"""
+    def __init__(self, name, row, is_tracked, recordedTime):
+        self.name = name
+        self.row = row
+        self.isTracked = False
+        self.recordedTime = 0
+
+
+tasks = ['dummy', 'dummy']  # creating a list for storing tasks with two dummy tasks (because first two rows are taken)
 rows = 2
 var = tk.IntVar()
 
 
+def timeTrack():
+    print(time.time())
+
+def select():
+    taskNumber = var.get()
+    tasks[taskNumber].is_tracked = True
+    if tasks[taskNumber].is_tracked:
+        timeTrack()
+        print("Task " + tasks[taskNumber].name + "is now tracked")
+
+
+
 def delTask():
-    print("DELETE!!")
+    print("DELETE TASK NUMBER "+ str(var.get()))
 
 
 def addTask():
 
-    if NewTaskEntry.get() == "": return
+    if TaskCreationEntry.get() == "":
+        return
     global rows
-    NewTask = tk.Label(master=frame, text=NewTaskEntry.get())
-    NewTask.grid(row=rows, column=0)
-    NewTaskEntry.delete(0, tk.END)
+    tasks.append(task(TaskCreationEntry.get(), rows))
+    NewTaskLabel = tk.Label(master=frame, text=TaskCreationEntry.get())
+    NewTaskLabel.grid(row=rows, column=0)
+    TaskCreationEntry.delete(0, tk.END)
 
     NewTaskTimer = tk.Label(master=frame, text="00:00:00")
     NewTaskTimer.grid(row=rows, column=1)
@@ -29,7 +54,7 @@ def addTask():
     TrackingLabel.grid(row = rows, column = 2)
     '''
 
-    NewTaskRadioBtn = tk.Radiobutton(master=frame, value = rows, variable = var)
+    NewTaskRadioBtn = tk.Radiobutton(master=frame, value = rows, variable = var, command = select)
     NewTaskRadioBtn.grid(row=rows, column=3)
 
     KillTaskBtn = tk.Button(master=frame, text = "Delete task", command=delTask)
@@ -42,16 +67,16 @@ frame = tk.Frame(master=window)
 frame.pack()
 
 WelcomeLabel = tk.Label(master=frame, text = "Welcome to WorkTracker, alpha version!")
-WelcomeLabel.grid(row = 0, column = 0)
+WelcomeLabel.grid(row=0, column=0)
 
-NewTaskLabel = tk.Label(master=frame, text="Input name of a new task to track:")
-NewTaskLabel.grid(row=1, column=0, sticky="e")
+TaskCreationLabel = tk.Label(master=frame, text="Input name of a new task to track:")
+TaskCreationLabel.grid(row=1, column=0)
 
-NewTaskEntry = tk.Entry(master=frame)
-NewTaskEntry.grid(row=1, column=1)
-NewTaskEntry.insert(0, "New Task")
+TaskCreationEntry = tk.Entry(master=frame)
+TaskCreationEntry.grid(row=1, column=1)
+TaskCreationEntry.insert(0, "New Task")
 
-NewTaskAddBtn = tk.Button(master=frame, text="Add", command=addTask)
-NewTaskAddBtn.grid(row=1, column=2)
+TaskCreationBtn = tk.Button(master=frame, text="Add", command=addTask)
+TaskCreationBtn.grid(row=1, column=2)
 
 window.mainloop()
